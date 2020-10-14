f6205436d533c3f5273672317a4f2103a3eceaa2
nhill <nhill@MININT-CU56C8E>
2020-10-14 17:03:08 +0100

    didnt test it properly!

--- Horus.Functions/Models/TrainingRequestMessage.cs
+++ Horus.Functions/Models/TrainingRequestMessage.cs
@@ -4,10 +4,18 @@ using System.Text;
 
 namespace Horus.Functions.Models
 {
-    public class TrainingResponseMessage
+    public class TrainingRequestMessage
     {
-        public List<TrainingRequestMessage> TrainingRequestMessages { get; set; }
-        public string ResponseMessage { get; set; }
+        public decimal AverageModelAccuracy { get; set; }
+        public string BlobSasUrl { get; set; }
+        public string BlobFolderName { get; set; }
+        public string IncludeSubFolders { get; set; }
+        public string UseLabelFile { get; set; }
+        public string DocumentFormat { get; set; }
+        public string ModelId { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime UpdatedDateTime { get; set; }
+        public string TrainingDocumentResults { get; set; }
     }
     // deployme
 }
--- Horus.Functions/Models/TrainingResponseMessage.cs
+++ Horus.Functions/Models/TrainingResponseMessage.cs
@@ -1,21 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Formatters.Internal;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Horus.Functions.Models
 {
-    public class TrainingRequestMessage
+    public class TrainingResponseMessage
     {
-        public decimal AverageModelAccuracy { get; set; }
-        public string BlobSasUrl { get; set; }
-        public string BlobFolderName { get; set; }
-        public string IncludeSubFolders { get; set; }
-        public string UseLabelFile { get; set; }
-        public string DocumentFormat { get; set; }
-        public string ModelId { get; set; }
-        public DateTime CreatedDateTime { get; set; }
-        public DateTime UpdatedDateTime { get; set; }
-        public string TrainingDocumentResults { get; set; }
+
+        public TrainingResponseMessage() 
+        {
+            TrainingRequestMessages = new List<TrainingRequestMessage>();
+        }
+        public List<TrainingRequestMessage> TrainingRequestMessages { get; set; }
+        public string ResponseMessage { get; set; }
     }
-    // deployme
 }
